06af19787681f58c2614c23418f022ccaa37689a
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-11-08 11:09:45 -0300

    Remove pastas indesejadas

--- meu_workspace/build/pond_nicola/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rizzi/Documentos/GitHub/ponderada-m8/meu_workspace/install/pond_nicola'
